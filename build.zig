d07a46aebaecd68a64a20190c1cfb4343c26a718
Ben Muthalaly <benmuthalaly@gmail.com>
2024-07-05 09:13:10 -0500

    Add 'check' step to build.zig

--- build.zig
+++ build.zig
@@ -36,18 +36,60 @@ pub fn build(b: *std.Build) void {
     // recompile the whole thing if not passed '-editor_only=true'
     if (!editor_only) {
         const exe = b.addExecutable(.{
-            .name = "no_hotreload",
-            .root_source_file = .{ .src_path = .{ .sub_path = "main.zig", .owner = b } },
+            .name = "BFIDE",
+            // .root_source_file = .{ .src_path = .{ .sub_path = "main.zig", .owner = b } },
+            .root_source_file = b.path("main.zig"),
             .target = target,
             .optimize = optimize,
         });
 
+        {
+            // codegen only runs if zig build sees a dependency on the binary output of
+            // the step. So we duplicate the build definition so that it doesn't get polluted by
+            // b.installArtifact.
+            const main = b.addExecutable(.{
+                .name = "BFIDE",
+                .root_source_file = b.path("main.zig"),
+                .target = target,
+                .optimize = optimize,
+            });
+
+            main.linkLibrary(raylib_dep.artifact("raylib"));
+            // main.linkLibrary(raylib_dep.artifact("rlgl"));
+            main.linkFramework("CoreVideo");
+            main.linkFramework("IOKit");
+            main.linkFramework("Cocoa");
+            main.linkFramework("GLUT");
+            main.linkFramework("OpenGL");
+            main.linkLibC();
+
+            const main_check = b.addExecutable(.{
+                .name = "BFIDE",
+                .root_source_file = b.path("main.zig"),
+                .target = target,
+                .optimize = optimize,
+            });
+
+            main_check.linkLibrary(raylib_dep.artifact("raylib"));
+            main_check.linkFramework("CoreVideo");
+            main_check.linkFramework("IOKit");
+            main_check.linkFramework("Cocoa");
+            main_check.linkFramework("GLUT");
+            main_check.linkFramework("OpenGL");
+            main_check.linkLibC();
+
+            const check = b.step("check", "Check if it compiles");
+            check.dependOn(&main_check.step);
+        }
+
         // Link to the Raylib and its required dependencies for macOS.
         exe.linkLibrary(raylib_dep.artifact("raylib"));
+        // exe.linkLibrary(raylib_dep.artifact("rlgl"));
         exe.linkFramework("CoreVideo");
         exe.linkFramework("IOKit");
         exe.linkFramework("Cocoa");
         exe.linkFramework("GLUT");
+        exe.linkFramework("OpenGL");
         exe.linkLibC();
 
         b.installArtifact(exe);
